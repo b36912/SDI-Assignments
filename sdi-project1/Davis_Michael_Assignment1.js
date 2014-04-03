7d2ca5a8a818fad36cb9294985aaf905ad6619b3
Michael Davis <mdavisii@fullsail.edu>
2014-04-02 20:04:50 -0400

    Update Davis_Michael_Assignment1.js

--- sdi-project1/Davis_Michael_Assignment1.js
+++ sdi-project1/Davis_Michael_Assignment1.js
@@ -1,1 +1,31 @@
 //alert("JavaScript works!");
+
+// Michael Davis
+// SDI 1403-02
+// Project 1
+
+
+/*
+Barber Shop
+*/
+
+// Internal Storage
+var namE      = "Eddie";
+var job       = "barber";
+var worksAt   = "the \"Davis Barber Shop\"";
+var workDist  = "20 minutes away";
+var workDays  = "Tuesday - Saturday";
+var agesUp16  = 10;
+var agesOlder = 20;
+var work      = true;
+
+//Outputs
+console.log("Hi my name is " + namE + ".");
+console.log("I am a professional " + job + ".");
+console.log("I work at " + worksAt + ".");
+console.log(namE + " lives " + workDist + " from his work place.");
+console.log(namE + " usually works " + workDays + ".");
+console.log("My prices for children up to ages 16 are " + agesUp16 + "dollars.");
+console.log("My prices for ages 17 and older are " + agesOlder + "dollars.");
+console.log("It's " + work + "I really love my job.");
+
