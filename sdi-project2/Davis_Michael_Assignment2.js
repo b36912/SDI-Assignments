533c1bb9ebacdb1b9f42a25a548ba9ba41c6ed67
Michael Davis <mdavisii@fullsail.edu>
2014-04-06 22:54:06 -0400

    Update Davis_Michael_Assignment2.js

--- sdi-project2/Davis_Michael_Assignment2.js
+++ sdi-project2/Davis_Michael_Assignment2.js
@@ -2,7 +2,7 @@
 
 // Michael Davis
 // SDI 1403-02
-// Project 1
+// Project 2
 
 
 /*
@@ -19,15 +19,6 @@ var agesUp16  = 10;
 var agesOlder = 20;
 var work      = true;
 
-//Outputs
-console.log("Hi my name is " + namE + ".");
-console.log("I am a professional " + job + ".");
-console.log("I work at " + worksAt + ".");
-console.log(namE + " lives " + workDist + " from his work place.");
-console.log(namE + " usually works " + workDays + ".");
-console.log("My prices for children up to ages 16 are " + agesUp16 + " dollars.");
-console.log("My prices for ages 17 and older are " + agesOlder + " dollars.");
-console.log("It's " + work + "I really love my job.");
 
 
 var wakeup = true
