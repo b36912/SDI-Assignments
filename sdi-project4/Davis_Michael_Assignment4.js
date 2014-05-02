d74c8b1c930dfc97ab7832044b73d9f35146ee97
Michael Davis <mdavisii@fullsail.edu>
2014-05-01 23:30:41 -0400

    Update Davis_Michael_Assignment4.js

--- sdi-project4/Davis_Michael_Assignment4.js
+++ sdi-project4/Davis_Michael_Assignment4.js
@@ -1,1 +1,28 @@
-//alert("JavaScript works!");
+/*
+Michael Davis
+SDI 1403-02
+PROJECT 4
+*/
+
+console.log("-Strings");
+//Verify telephone number (true or false)
+var cellNumReturn;
+
+var verifyTelNum = function (num) {
+
+    var telLength = num.length;
+   
+    for (var i = 0; i < telLength; i++) {
+      
+        if (num > "cell") {
+            
+            console.log("The telephone number is true.");
+            
+        } else {
+            console.log("The telephone number is false.");
+        }
+    };
+           
+};       
+cellNumReturn = verifyTelNum("215-287-3163");
+
